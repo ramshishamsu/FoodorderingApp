52c8474dd4624b36dd365647c44f8dbcfe6c527f
ramshishamsu <ramshishamsu22@gmail.com>
2025-10-13 20:19:26 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/project">
       <App />
     </BrowserRouter>
   </Provider>
