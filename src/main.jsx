603914e710d1026d19fbfed0b9dcc9c73a323a98
ramshishamsu <ramshishamsu22@gmail.com>
2025-10-14 17:43:25 +0530

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import store from "./store";  // make sure this matches your store.js location
 import App from "./App";
 import "./index.css";
+import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
+    <HashRouter>
     <BrowserRouter basename="/FoodorderingApp">
       <App />
     </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
