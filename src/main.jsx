8be6193ef6f87b75cc321abeee4ef7ea34d3a330
ramshishamsu <ramshishamsu22@gmail.com>
2025-10-14 18:06:51 +0530

    Updated project files / fixed deployment paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import store from "./store";  // make sure this matches your store.js location
 import App from "./App";
 import "./index.css";
-import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <HashRouter>
-    <BrowserRouter basename="/FoodorderingApp">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
-    </HashRouter>
   </Provider>
 );
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [ tailwindcss(),react()],
-     base: '/FoodorderingApp/'
-    
+    build: {
+    outDir: 'dist',
+    }
 })
